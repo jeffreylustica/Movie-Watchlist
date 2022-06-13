1b2bd421737fcdfbb15f9f96306de7d0fc71f592
jeffnew1 <jefflustica2@gmail.com>
2022-06-13 15:40:36 +0800

    Refactor Pagination
    Refactor Pagination

--- js/pagination.js
+++ js/pagination.js
@@ -1,21 +1,27 @@
 import {movieListContainerEl} from './variables.js'
 import {fetchSearchInput, renderHtmlReturn} from './utils.js'
 
-function createPagination() {
-    const paginationEl = document.querySelector('#pagination')
-    paginationEl.style.display = "block"
-    paginationEl.innerHTML = ''
+let highestPageNumber = 5
+let lowestPageNumber = 1
 
-    for (let i = 0; i < 5; i++) {
-        paginationEl.innerHTML += `
-        <button class="page-number">${i + 1}</button>
-        `      
-    }   
+function createPagination() {
+    
+    renderPagination(highestPageNumber)
 
     const pageNumberButton = document.querySelectorAll('.page-number')
     pageNumberButton.forEach(button => {
         button.addEventListener('click' , (e) => {
-            const pageNumber = e.target.textContent
+            const pageNumber = e.target.dataset.index
+            if (pageNumber > highestPageNumber) {                
+                highestPageNumber += 5
+                lowestPageNumber +=5
+                renderPagination(highestPageNumber)
+            } else if (pageNumber < lowestPageNumber) {
+                highestPageNumber -= 5
+                lowestPageNumber -=5
+                renderPagination(highestPageNumber)
+            }
+            
             const searchInput = document.querySelector('#search-input')
 
             movieListContainerEl.innerHTML = `
@@ -36,6 +42,27 @@ function createPagination() {
 
         })
     })
+}
+
+function renderPagination(highestPageNumberParam) {
+    console.log(highestPageNumberParam)
+    const paginationEl = document.querySelector('#pagination')
+    paginationEl.style.display = "block"
+    paginationEl.innerHTML = ''
+    let pageNumberButtonsHtml = ""
+    let paginationHtml = ""
+
+    for (let i = highestPageNumberParam - 5; i < highestPageNumberParam; i++) {
+        pageNumberButtonsHtml += `
+        <button class="page-number" data-index= ${i + 1} >${i + 1}</button>
+        `      
+    } 
+    paginationHtml = `
+        ${highestPageNumberParam !== 5 ? `<button class="page-number" data-index=${highestPageNumberParam - 5}>&laquo;</button>` : ""}  
+        ${pageNumberButtonsHtml}
+        <button class="page-number" data-index=${highestPageNumberParam + 1}>&raquo;</button>
+    `
+    paginationEl.innerHTML = paginationHtml
 }
 
 
